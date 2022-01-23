2018c893ee466c077c4f1c3f7c4f81bb85f342a0
chaitanyapottam <kchaitupottam@gmail.com>
2022-01-23 17:23:32 +0530

    added books an books_author and defined the relationship in AppDbContext file OnModelCreating methos.

---
+++ my-books/Data/Models/Book_Author.cs
@@ -0,0 +1,13 @@
+﻿namespace my_books.Data.Models
+{
+    public class Book_Author
+    {
+        public int Id { get; set; }
+
+        //Navigation Properties
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
