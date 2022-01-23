28c25e851f1a306f7f816cb73ba73084b8f4fa78
chaitanyapottam <kchaitupottam@gmail.com>
2022-01-23 17:51:03 +0530

    Added publisher and autor controllers

---
+++ my-books/Data/ViewModels/AuthorVM.cs
@@ -0,0 +1,10 @@
+﻿using my_books.Data.Models;
+using System.Collections.Generic;
+
+namespace my_books.Data.ViewModels
+{
+    public class AuthorVM
+    {
+        public string FullName { get; set; }
+    }
+}
